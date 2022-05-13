11e272e130e723974f597f1edb3db318f0abf1a6
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2022-05-12 22:47:04 -0300

    feat: profile screen

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -108,6 +108,10 @@ const AppProvider = ({ children }) => {
 		removeUserFromLocalStorage()
 	}
 
+	const updateUser = async (currentUser) => {
+		console.log(currentUser)
+	}
+
 	return (
 		<AppContext.Provider
 			value={{
@@ -117,6 +121,7 @@ const AppProvider = ({ children }) => {
 				loginUser,
 				toggleSideBar,
 				logoutUser,
+				updateUser,
 			}}
 		>
 			{children}

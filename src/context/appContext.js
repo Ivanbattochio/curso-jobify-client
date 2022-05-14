33e0d1c838a40f8d91199aa5d9cac330247cfd3f
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2022-05-14 18:55:14 -0300

    fix: updteuser error alert msg, logout if no token

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -44,7 +44,7 @@ const AppProvider = ({ children }) => {
 
 	authFetch.interceptors.request.use(
 		(config) => {
-			config.headers.common["Authorization"] = `Bearer ${state.token}`
+			//config.headers.common["Authorization"] = `Bearer ${state.token}`
 			return config
 		},
 		(error) => {
@@ -58,7 +58,7 @@ const AppProvider = ({ children }) => {
 		},
 		(error) => {
 			console.log(error.response)
-			if (error.response.status === 401) console.log("AUTH ERROR")
+			if (error.response.status === 401) logoutUser()
 			return Promise.reject(error)
 		}
 	)
@@ -152,10 +152,12 @@ const AppProvider = ({ children }) => {
 			})
 			addUserToLocalStorage({ user, location, token })
 		} catch (error) {
-			dispatch({
-				type: UPDATE_USER_ERROR,
-				payload: error.response.data.msg,
-			})
+			if (!error.response.status === 401) {
+				dispatch({
+					type: UPDATE_USER_ERROR,
+					payload: { msg: error.response.data.msg },
+				})
+			}
 		}
 		clearAlert()
 	}

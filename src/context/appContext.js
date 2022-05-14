f9e0577090e29733788b2adc51163a699c985775
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2022-05-14 18:24:03 -0300

    feat: axios interceptor in updateUser req

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -35,6 +35,31 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState)
 
+	const authFetch = axios.create({
+		baseURL: "/api/v1",
+	})
+
+	authFetch.interceptors.request.use(
+		(config) => {
+			//config.headers.common["Authorization"] = `Bearer ${state.token}`
+			return config
+		},
+		(error) => {
+			return Promise.reject(error)
+		}
+	)
+
+	authFetch.interceptors.response.use(
+		(response) => {
+			return response
+		},
+		(error) => {
+			console.log(error.response)
+			if (error.response.status === 401) console.log("AUTH ERROR")
+			return Promise.reject(error)
+		}
+	)
+
 	const displayAlert = () => {
 		dispatch({ type: DISPLAY_ALERT })
 		clearAlert()
@@ -109,7 +134,16 @@ const AppProvider = ({ children }) => {
 	}
 
 	const updateUser = async (currentUser) => {
-		console.log(currentUser)
+		console.log(state.token, currentUser)
+		try {
+			const { data } = await authFetch.patch(
+				"/auth/updateUser",
+				currentUser
+			)
+			console.log(data)
+		} catch (error) {
+			//console.log(error)
+		}
 	}
 
 	return (
